228e067692bb7843c01b52dc5be6864403ec68dc
ragnaringi <ragnar.hrafnkelsson@gmail.com>
2018-04-20 18:22:56 +0100

    Public headers import

--- RFYAppKit/RFYAppKit.h
+++ RFYAppKit/RFYAppKit.h
@@ -16,7 +16,14 @@ FOUNDATION_EXPORT const unsigned char RFYAppKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <RFYAppKit/PublicHeader.h>
 
-#import "RFYAudioFilePlayer.h"
-#import "RFYAudioQueuePlayer.h"
-#import "RFYAudioStemsPlayer.h"
+#import <RFYAppKit/RFYUtils.h>
+#import <RFYAppKit/NSArray+RFYUtilities.h>
+
+#import <RFYAppKit/RFYAudioUtils.h>
+#import <RFYAppKit/RFYAudioFilePlayer.h>
+#import <RFYAppKit/RFYAudioQueuePlayer.h>
+#import <RFYAppKit/RFYAudioStemsPlayer.h>
+
+#import <RFYAppKit/ObjectiveSugar.h>
+#import <RFYAppKit/TPCircularBuffer+AudioBufferList.h>
 
